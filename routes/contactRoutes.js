3d3b8a19acb9daf3a5f127da8ef4b0edf986c7c4
yashgupta18 <18guptayash@gmail.com>
2023-07-08 00:17:50 +0530

    first commit

---
+++ routes/contactRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
+
+// use validateToken middleware for all routes to validate access token
+router.use(validateToken);
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
+
+module.exports = router;
